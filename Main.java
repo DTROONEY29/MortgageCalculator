bc6740274603032511e7223f8f0a473b0c6ac8b5
Daniel Rooney <34038475+DTROONEY29@users.noreply.github.com>
2020-02-04 16:18:42 +0000

    extraxted methods, refactored code

--- Main.java
+++ Main.java
@@ -1,75 +1,102 @@
+package com.company;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+
 public class Main {
+    final static int monthsInAYear = 12;
+    final static int percent = 100;
 
     public static void main(String[] args) {
 
-        // Variables
+        System.out.println("Welcome to the mortgage Calculator!");
 
-        final int MONTHS_IN_YEAR = 12;
-        final int PERCENT = 100;
-        double size = 0;
-        double interestRate = 0.0;
-        double monthlyRate = 0.0;
-        double totalPayments = 0.0;
-        double onePlusMonthlyRate = 0.0;
-        // Welcome message
-        System.out.println("Welcome to the simple mortgage calculator!");
-        //create scanner
-        Scanner scanner = new Scanner(System.in);
-        // prompt user
-        while (true) {
-            System.out.print("How big is the loan? (£1K - £1M): ");
-            size = scanner.nextInt();
+        double principle = readNumber("Principal: ", 1000, 1_000_000);
 
-            // If user enters value out of range, display prompt:
-            if (size >= 1000 && size <= 1_000_000)
-                break;
-            System.out.println("Please enter a value between 1000 and 1 million.");
+        double annualInterestRate = readNumber("Annual Interest Rate: ", 1, 30);
 
+        int years = (int) readNumber("Period in years: ", 1, 30);
 
-        }
-        while (true) {
-//  Prompt user
-            System.out.print("Enter annual interest rate:");
-
-            interestRate = scanner.nextDouble();
-
-
-            if (interestRate >= 1 && interestRate <= 25) {
-                // Calculate monthly interest rate.
-                monthlyRate = (interestRate / MONTHS_IN_YEAR) / PERCENT;
-                break;
-            }
-            // If user enters value out of range, display prompt:
-
-            System.out.println("Please enter a value between 1 and 25.");
-
-        }
-
-
-        while (true) {
-            //Prompt user
-            System.out.print("How long is the period of repayment (in years): ");
-            double repayPeriod = scanner.nextDouble();
-            if (repayPeriod >= 3 && repayPeriod <= 50) {
-                // Calculate the number of monthly payments
-                totalPayments = repayPeriod * MONTHS_IN_YEAR;
-                break;
-            }
-            // If user enters value out of range, display prompt:
-
-            System.out.println("Please enter a value between 3 and 50. ");
-        }
-
-        // calculate expression (1+R)^n used in equation for cleaner code
-        onePlusMonthlyRate = Math.pow(1 + monthlyRate, totalPayments);
-        //Calculate monthly mortgage payment
-        double monthlyMortgagePayment = size * ((monthlyRate * onePlusMonthlyRate) / (onePlusMonthlyRate - 1));
-        // convert to currency format
-        String mortgageReadable = NumberFormat.getCurrencyInstance().format(monthlyMortgagePayment);
-        System.out.println("Your monthly payment will be: " + mortgageReadable);
+        printMortgage(principle, annualInterestRate, years);
+        printPaymentSchedule(principle, annualInterestRate, years);
 
     }
+
+    private static void printMortgage(double principle, double annualInterestRate, int years) {
+        double result = calculateMortgage(principle, annualInterestRate, years);
+        System.out.println("----------");
+        System.out.println("MORTGAGE");
+        System.out.println("----------");
+        System.out.println("Your mortgage payment is: " + NumberFormat.getCurrencyInstance().format(result));
+    }
+
+    private static void printPaymentSchedule(double principle, double annualInterestRate, int years) {
+        System.out.println();
+        System.out.println("--- PAYMENTS ---");
+        System.out.println("-------------");
+        for (int month = 1; month <= years * monthsInAYear; month++) {
+
+            double balance = remainingBalance(principle, annualInterestRate, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextDouble();
+
+            if (value >= min && value <= max)
+                break;
+            System.out.println("PLease enter an amount between" + min + " and " + max);
+
+        }
+
+        return value;
+    }
+
+    public static double calculateMortgage(
+            double principle,
+            double annualInterestRate,
+            int years) {
+
+        int lengthInMonths = years * monthsInAYear;
+        double monthlyInterestRate = annualInterestRate / percent / monthsInAYear;
+
+        double result = principle * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, lengthInMonths)) /
+                (Math.pow(1 + monthlyInterestRate, lengthInMonths) - 1);
+        return result;
+    }
+
+    public static double remainingBalance(
+            double principle,
+            double annualInterestRate,
+            int years,
+            int numberOfPaymentsMade) {
+
+        int lengthInMonths = years * monthsInAYear;
+        double monthlyInterestRate = annualInterestRate / percent / monthsInAYear;
+
+        double balance = principle * (Math.pow(1 + monthlyInterestRate, lengthInMonths) - Math.pow(1 + monthlyInterestRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterestRate, lengthInMonths) - 1);
+
+
+        return balance;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
